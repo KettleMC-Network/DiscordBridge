7432419aeb212bcc24702be54749bb8493ec5686
UeberallGebannt <paulmertens63@gmail.com>
2022-01-21 13:33:33 +0100

    Added startup and shutdown message

--- src/net/kettlemc/discordbridge/DiscordBridge.java
+++ src/net/kettlemc/discordbridge/DiscordBridge.java
@@ -6,6 +6,7 @@ import net.kettlemc.discordbridge.listener.AsyncChatListener;
 import net.kettlemc.discordbridge.listener.JoinQuitListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class DiscordBridge extends JavaPlugin {
 
@@ -18,14 +19,21 @@ public class DiscordBridge extends JavaPlugin {
             this.bot = new DiscordBot(this);
         else
             this.getLogger().warning("Please enter a token!");
-        this.registerListener();
+        this.registerBukkit();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                bot.sendMessage(configuration.channel, configuration.startupMessage);
+            }
+        }.runTaskLaterAsynchronously(this, 40L);
     }
 
     public void onDisable() {
+        this.bot.sendMessage(configuration.channel, this.configuration.shutdownMessage);
         this.bot.shutdown();
     }
 
-    private void registerListener() {
+    private void registerBukkit() {
         Bukkit.getPluginManager().registerEvents(new AsyncChatListener(this), this);
         Bukkit.getPluginManager().registerEvents(new JoinQuitListener(this), this);
     }
