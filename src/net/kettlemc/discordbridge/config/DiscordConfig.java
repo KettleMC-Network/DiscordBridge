a33cb1d363f7e564d798ecd4a0022a57dc4fe1f7
Fernand Adam <fernandadam@posteo.de>
2022-10-20 22:05:28 +0200

    added the default config, changed path of DiscordConfig.java

--- src/net/kettlemc/discordbridge/config/DiscordConfig.java
+++ src/net/kettlemc/discordbridge/config/DiscordConfig.java
@@ -9,10 +9,10 @@ import net.kettlemc.konfiguration.entry.StringConfigEntry;
 
 public class DiscordConfig {
 
-    private static Configuration CONFIGURATION = new Configuration(DiscordBridge.getInstance().getDataFolder().toPath().resolve("config.json"));
+    private static Configuration CONFIGURATION = new Configuration(DiscordBridge.getInstance().getDataFolder().toPath().resolve("resources/config.json"));
 
     public static StringConfigEntry DISCORD_TOKEN = new StringConfigEntry(CONFIGURATION, "YOUR TOKEN", "settings.discord.token");
-    public static LongConfigEntry DISCORD_CHANNEL_ID = new LongConfigEntry(CONFIGURATION, -1l, "settings.discord.channel-id");
+    public static LongConfigEntry DISCORD_CHANNEL_ID = new LongConfigEntry(CONFIGURATION, -1L, "settings.discord.channel-id");
     public static StringConfigEntry DISCORD_BOT_STATUS = new StringConfigEntry(CONFIGURATION, " with %online% players", "settings.discord.status");
     public static BooleanConfigEntry DISCORD_DISABLE_FORMATTING = new BooleanConfigEntry(CONFIGURATION, true, "settings.discord.disable-formatting");
 
